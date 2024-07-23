974412e1188f8fd945c57f2303193f62423d1acd
rjsoloaga <javiersoloaga5@gmail.com>
2024-07-23 11:35:38 -0300

    Primer Hola Mundo

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+package HELLOWORLD;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Informatorio Java");
+    }
+}
